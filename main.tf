786ae0fbfc0524f36f5d15461db818f04edc97a6
mariobignami <marioaugusto23@gmail.com>
2024-04-10 20:29:24 -0300

    adjusting 14

--- main.tf
+++ main.tf
@@ -1,63 +1,69 @@
 variable "HMAC_ACCESS_KEY" {
-  description = "The HMAC access key for GCP"
-  type        = string
+    description = "The HMAC access key for GCP"
+    type        = string
 }
 
 variable "HMAC_SECRET" {
-  description = "The HMAC secret for GCP"
-  type        = string
-  sensitive   = true
+    description = "The HMAC secret for GCP"
+    type        = string
+    sensitive   = true
+}
+
+provider "google" {
+    credentials = file("creds.json")
+    project     = "devops-util-test"
+    region      = "us-central1"
 }
 
 resource "google_container_cluster" "primary" {
-  name               = "node-demo-k8s"  # cluster name
-  location           = "us-central1-c"
-  initial_node_count = 4               # number of node (VMs) for the cluster
+    name               = "node-demo-k8s"  # cluster name
+    location           = "us-central1-c"
+    initial_node_count = 4               # number of node (VMs) for the cluster
 
-  master_auth {
-    client_certificate_config {
-      issue_client_certificate = false
-    }
-  }
-
-  provisioner "local-exec" {
-    command = "gcloud container clusters get-credentials node-demo-k8s --zone us-central1-c --project devops-util-test"
-  }
-
-  node_config {
-    preemptible  = true
-    machine_type = "e2-micro"
-
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/compute",
-      "https://www.googleapis.com/auth/devstorage.read_only",
-      "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring",
-    ]
-
-    metadata = {
-      disable-legacy-endpoints = "true"
+    master_auth {
+        client_certificate_config {
+            issue_client_certificate = false
+        }
     }
 
-    tags = ["node-demo-k8s"]
-  }
+    provisioner "local-exec" {
+        command = "gcloud container clusters get-credentials node-demo-k8s --zone us-central1-c --project devops-util-test"
+    }
 
-  timeouts {
-    create = "45m" 
-    update = "60m"
-  }
+    node_config {
+        preemptible  = true
+        machine_type = "e2-micro"
+
+        oauth_scopes = [
+            "https://www.googleapis.com/auth/compute",
+            "https://www.googleapis.com/auth/devstorage.read_only",
+            "https://www.googleapis.com/auth/logging.write",
+            "https://www.googleapis.com/auth/monitoring",
+        ]
+
+        metadata = {
+            disable-legacy-endpoints = "true"
+        }
+
+        tags = ["node-demo-k8s"]
+    }
+
+    timeouts {
+        create = "45m" 
+        update = "60m"
+    }
 }
 
 resource "google_compute_firewall" "nodeports" {
-  name    = "node-port-range"
-  network = "default"
+    name    = "node-port-range"
+    network = "default"
 
-  allow {
-    protocol = "tcp"
-    ports    = ["30000-32767", "80", "443", "8080", "22"]
-  }
-  allow {
-    protocol = "icmp"
-  }
-  source_ranges = ["0.0.0.0/0"]
+    allow {
+        protocol = "tcp"
+        ports    = ["30000-32767", "80", "443", "8080", "22"]
+    }
+    allow {
+        protocol = "icmp"
+    }
+    source_ranges = ["0.0.0.0/0"]
 }
