63404ad614437b1fb056907b653254045227b743
mariobignami <marioaugusto23@gmail.com>
2024-04-10 19:34:14 -0300

    adjusting 7

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
-variable "HMAC_ACCESS_KEY" {
+variable "TF_VAR_HMAC_ACCESS_KEY" {
   description = "The HMAC access key for GCP"
   type        = string
 }
 
-variable "HMAC_SECRET" {
+variable "TF_VAR_HMAC_SECRET" {
   description = "The HMAC secret for GCP"
   type        = string
   sensitive   = true
